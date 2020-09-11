7acdb71cdf9759f35d3a3f0b7b23e901d176027f
JokerOfRivia <70218072+JokerOfRivia@users.noreply.github.com>
2020-09-11 09:24:36 -0700

    Update Decoder.java

--- Decoder.java
+++ Decoder.java
@@ -2,7 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class Decoder {
-	public void decodeFile(File encoded) {
-		
+	public void decodeFile(File encoded) throws IOException {
+		BufferedReader br=new BufferedReader(encoded);
+		ArrayList<Integer> codes=new ArrayList<Integer>;
+		int temp;
+		while ((temp=br.read())!=-1){
+			codes.add(temp);
+		}
+		br.close();
+		String output="";
+		for (int i=0;i<codes.size();i++){
+			output+=dictionary.get(codes.get(i));
+		}
+		BufferedWriter bw=new BufferedWriter(new FileWriter());
+		bw.append(output);
+		bw.close();
 	}
 }
